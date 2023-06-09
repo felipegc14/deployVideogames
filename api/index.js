8e67e493dbb13a00ed0cb1a7de75f5b4a305454f
felipegc14 <felipegc000@gmail.com>
2023-06-09 20:51:31 -0300

    deployyyy

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log('%s listening at' ,process.env.PORT); // eslint-disable-line no-console
+  server.listen(process.env.DB_PORT, () => {
+    console.log('%s listening at' ,process.env.DB_PORT); // eslint-disable-line no-console
   });
 });
