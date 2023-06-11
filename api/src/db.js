87b913f3cf7521cccb41553173709fc77f3e0e09
felipegc14 <felipegc000@gmail.com>
2023-06-11 11:35:53 -0300

    lastCommit

--- api/src/db.js
+++ api/src/db.js
@@ -6,11 +6,11 @@ const path = require('path');
 const Genres = require('./models/Genres') */
 //const modelVideogame = require('./Models/Videogame');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
 } = process.env;
 
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
